a7169d652d847214b7b8190f9f34f1411990a0d6
jared frankel <jaredfrankel10@gmail.com>
2018-02-05 23:08:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
-ReactDOM.render(
-  meInReact,
-  document.getElementById('global')
-);
 
-export default meInReact
-*/
+ // <div class="me">
+ //   <h1>An Awesome Person</h1>
+ //   <p>Who is learning React</p>
+ //
+ //   <ul class="my-interests">
+ //     <li>JavaScript</li>
+ //     <li>React</li>
+ //     <li>Movies</li>
+ //     <li>Ice cream</li>
+ //   </ul>
+ // </div>
+
+ const t = React.createElement(`h1`,{key: `title`}, `An Awesome Person`)
+
+ const p = React.createElement(`p` ,{key: `paragraph`}, `Who is learning React`)
+
+   const interests = [`JavaScript`, `React`, `Movies`, `Ice cream`]
+
+   let counter = 0
+   const items = interests.map((interest) => {
+     return React.createElement(`li`, {key: `interest${++counter}`, className: `item`}, `${interest}`)
+   })
+
+ const listToRender = React.createElement(`ul`, {key: `list`, className: `my-interests`}, [...items])
+
+
+ const meInReact = React.createElement(`div`, {key: `meInReact`, className: `me`}, [t, p, listToRender])
+ ReactDOM.render(
+    meInReact,
+    document.getElementById('global')
+  );
+
+  export default meInReact
